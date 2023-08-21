00eb06c7001df9fb1e368a65d969328cb229e216
YohanNzi <yohannzi.pro@gmail.com>
2023-08-21 10:27:29 +0200

    first recommit

---
+++ assets/data/users.js
@@ -0,0 +1,65 @@
+export default [
+  {
+    id: '1',
+    name: 'Machu Picchu',
+    image:
+      'https://www.intrepidtravel.com/adventures/wp-content/uploads/2018/06/1.-Intrepid-Travel-peru_machupicchu.jpg',
+    location: 'Peru',
+    bio: 'Cité inca construite au XVème siècle dans les montagnes des Andes au Pérou',
+  },
+  {
+    id: '2',
+    name: 'Taj Mahal',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Taj_Mahal%2C_Agra%2C_India_edit3.jpg/1280px-Taj_Mahal%2C_Agra%2C_India_edit3.jpg', 
+    location: 'India',
+    bio: 'Nouvelle merveille du monde, le Taj Mahal est un immense mausolée funéraire de marbre blanc qui attire chaque année près de 3 millions de visiteurs dans le nord de l Inde. Construit au XVIIe siècle par l empereur moghol Shah Jahan, il est dédié à son épouse Mumtaz Mahal, morte en couches. Le Taj Mahal est inscrit au patrimoine mondial de l Unesco depuis 1983.'
+  },
+  {
+    id: '3',
+    name: 'Statue du Christ Redempteur',
+    image:
+      'https://cdn.britannica.com/54/150754-050-5B93A950/statue-Christ-the-Redeemer-Rio-de-Janeiro.jpg',
+    location: 'Brazil',
+    bio: 'Classé monument historique depuis 1973, le Christ du Corcovado est l un des endroits touristiques les plus fréquentés de Rio avec 750 000 visiteurs par an',
+  },
+  {
+    id: '4',
+    name: 'Marie-Galante',
+    image:
+      'https://media.routard.com/image/81/5/baie-saintes-guadeloupe-ile.1515815.w630.jpeg',
+    location: 'Guadeloupe',
+    bio: 'La Guadeloupe : une île magique avec les longues plages blanches de Sainte-Anne, les plages noires de Trois-Rivières, les anses bordées de mangroves et de cocotiers.',
+  },
+  {
+    id: '5',
+    name: 'La Gomera et El Hierro, les Canaries confidentielles',
+    image:
+      'https://media.routard.com/image/02/9/canaries-el-hierro-la-frontera.1598029.w630.jpg',
+    location: 'Iles Canaries',
+    bio: 'La Gomera possède un parc national dont la flore ravira les amoureux de nature. El Hierro semble restée à l’état sauvage avec ses montagnes de roches volcaniques, pinèdes et forêts de lauriers. De quoi réveiller l\’explorateur qui sommeille en nous !',
+  },
+  {
+    id: '6',
+    name: 'Salzbourg',
+    image:
+      'https://media.routard.com/image/92/0/hallstatt.1611920.w630.jpg',
+    location: 'Austria',
+    bio: 'Ville natale de Mozart, Salzbourg compte parmi les cités les plus séduisantes et singulières d’Europe centrale, avec un centre historique largement piétonnier, enserré entre 2 collines et dominé par la silhouette romantique d’une forteresse médiévale.',
+  },
+  {
+    id: '7',
+    name: 'Les Highlands',
+    image:
+      'https://media.routard.com/image/86/7/ecosse-loch-ness-urquhart.1528867.w630.jpg',
+    location: 'Ecosse',
+    bio: 'Les Highlands, c’est la région la plus sauvage et la plus spectaculaire d’Écosse. Les paysages y sont grandioses, entre montagnes, lochs, landes et tourbières. Les Highlands sont aussi le berceau de la culture écossaise, avec ses châteaux, ses distilleries de whisky, ses clans et ses légendes.',
+  },
+  /*{
+    id: '5',
+    name: 'Vadim Savin',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
+    bio: 'Hola',
+  },*/
+];
